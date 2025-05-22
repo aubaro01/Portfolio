2dfca92232e9763a2f8cd24ed14499fcc58410b0
Cláudio Barroso <109551603+aubaro01@users.noreply.github.com>
2025-05-22 11:59:33 +0100

    Update App.jsx

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter  as Router, Routes, Route } from 'react-router-dom';
 import Footer from './components/footer';
 import Header from './components/header'
 import About from './components/about';
