dfea9bfbfbbf47b45b3346cb3f198bc77a97d00e
Cláudio Barroso <109551603+aubaro01@users.noreply.github.com>
2025-05-27 11:40:25 +0100

    Update data.js

--- web/src/data/data.js
+++ web/src/data/data.js
@@ -1,42 +1,24 @@
 export const posts = [
     {
         id: '1',
-        title: 'Projeto Sav',
-        subtitle: 'Meu primeiro Projeto em React.js com Express.js e MongoDB',
-        content: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium...Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium...Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium...Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium...Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium...',
+        title: 'Projeto Socorros S.A.V.',
+        subtitle: 'Meu primeiro projeto em React.js com Express.js e MongoDB',
+        content: 'Este primeiro projeto foi uma experiência incrível. Além de ser uma aplicação real, ele me ajudou a aprofundar meus conhecimentos na framework. Ainda há pontos que preciso melhorar dentro do projeto, mas, tirando isso, foi super interessante. Aprendi, por exemplo, a importância de manter um plano de desenvolvimento e de ter uma boa comunicação com o cliente.',
         images: [
             'https://raw.githubusercontent.com/aubaro01/SocorrosSav/main/public/sav.ico',
-
         ],
-        code: [
-            {
-                
-            }
-        ]
+        code: []
     },
     {
-        id: '2',
+        id: '3',
         title: 'PC AUTO',
         subtitle: 'Sistema de gestão para oficina mecânica em Python',
-        content: '"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium...',
+        content: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium...', 
         images: [
             'https://exemplo.com/pcauto-diagrama.png',
             'https://exemplo.com/pcauto-interface.jpg'
         ],
-        code: [
-            {
-
-            }
-        ]
-    },
-    {
-        id: '3',
-        title: 'Internship',
-        subtitle: 'Experiências de estágio em desenvolvimento',
-        content: 'But I must explain to you how all this mistaken idea...',
-        images: [],
-        code: [
-            {
+        code: [{
                 language: 'sql',
                 content: `-- Exemplo de query otimizada
 SELECT clientes.nome, COUNT(ordens_servico.id) AS total_os
@@ -45,7 +27,14 @@ LEFT JOIN ordens_servico
   ON clientes.id = ordens_servico.cliente_id
 GROUP BY clientes.nome
 HAVING total_os > 5;`
-            }
-        ]
+            }]
+    },
+    {
+        id: '2',
+        title: 'Internship',
+        subtitle: 'Experiência de estágio em Software Development',
+        content: 'Tive o prazer de fazer o meu estágio, pós curso em uma empresa dentro da area. Dentro dessa empresa aprendi diversas coisas e tive o prazer de trabalhar com diversas tecnologias. ', 
+        images: [],
+        code: []
     }
 ];
